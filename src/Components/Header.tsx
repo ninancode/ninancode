86fa7db142436cba5bf6cd17f04a2c485b9ba3b5
nina.n.code <nina.n.code@revature.net>
2021-09-03 16:28:55 -0400

    started some pages

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+import Logo from '../assets/N Logo.png'
+
+export function Header() {
+    return (
+        <>
+            <img src={Logo} id='logo'/>
+        </>
+    )
+}
---
+++ src/Components/Pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../../assets/N Logo.png'
+
+export function Home() {
+    return (
+        <>
+        <p>Home</p>
+        </>
+    )
+}
